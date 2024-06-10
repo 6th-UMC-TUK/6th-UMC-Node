b1803826bd6541f5893e65b9ea8de387fe8cc985
gs0428 <lider28@naver.com>
2024-06-10 21:15:07 +0900

    feat: week10 mission setup

--- lany/week10/swagger/swagger.config.js
+++ lany/week10/swagger/swagger.config.js
@@ -2,4 +2,23 @@ import path from "path";
 import YAML from "yamljs";
 import { cwd } from "process";
 
-export const swaggerSpec = YAML.load(path.join(cwd(), "/swagger/store.swagger.yaml"));
+const missionSwagger = YAML.load(path.join(cwd(), "/swagger/mission.swagger.yaml"));
+const storeSwagger = YAML.load(path.join(cwd(), "/swagger/store.swagger.yaml"));
+
+export const swaggerSpec = {
+  openapi: "3.0.0",
+  info: {
+    title: "UMC week10 mission",
+    version: "1.0.0",
+  },
+  paths: {
+    ...missionSwagger.paths,
+    ...storeSwagger.paths,
+  },
+  components: {
+    schemas: {
+      ...missionSwagger.components?.schemas,
+      ...storeSwagger.components?.schemas,
+    },
+  },
+};
